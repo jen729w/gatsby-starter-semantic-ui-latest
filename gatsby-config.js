2570ec5d0ea4fded783891e6229a508dd98a0e4a
John Noble <jn@jnoble.net>
2019-09-07 18:28:43 +1000

    System font stack

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Semantic UI Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-less'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-less',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+        omitGoogleFont: true,
+      },
+    },
+  ],
+};
